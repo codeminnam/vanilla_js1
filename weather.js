3991c30a7d2f68b0919a1ca5889f2e457d2b05c6
cineminnam <cine.minnam@gmail.com>
2020-04-08 00:06:37 +0900

    #3.9 Getting the Weather part One (Geolocation)
    #3.9 Getting the Weather part One (Geolocation)

---
+++ weather.js
@@ -0,0 +1,39 @@
+const API_KEY = "da2cd998d9399a751bf1e9c2edc45b6e";
+const COORDS = 'coords';
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+}
+
+function handleGeoError(){
+    console.log("can't access");
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords(){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null){
+        askForCoords();
+    }else{
+        //getWeather
+    }
+}
+
+function init(){
+    loadCoords();
+}
+
+init();
