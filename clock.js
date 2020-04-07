54600afec2c5c905091421eac4649f82489fcfd7
cineminnam <cine.minnam@gmail.com>
2020-04-07 12:13:01 +0900

    #3.2 Making a JS Clock part Two
    #3.2 Making a JS Clock part Two

--- clock.js
+++ clock.js
@@ -6,11 +6,12 @@ function getTime(){
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours} : ${minutes} : ${seconds}`;
-
+    clockTitle.innerText = 
+    `${hours > 9  ? `${hours}`:`0${hours}`} : ${minutes >9 ? `${minutes}`:`0${minutes}`} : ${seconds > 9 ? `${seconds}`:`0${seconds}`}`;
 }
 
 function init(){
     getTime();
+    setInterval(getTime, 1000);
 }
 init()
